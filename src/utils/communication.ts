8c7eb1a20623e0f130463691a92344975ba6a7ff
Takeshi Iwana <iwatakeshi@gmail.com>
2019-11-09 00:33:18 -0700

    Refactor communication
    Use axios and clean up code

--- src/utils/communication.ts
+++ src/utils/communication.ts
@@ -1,69 +1,46 @@
-import axios from 'axios'
-import request = require('request')
+import axios, { AxiosResponse } from 'axios'
 
 import Config from './config'
 import { ForbiddenError, InternalServerError, UnauthorizedError, UnknownError } from './error'
 
+axios.defaults.maxRedirects = 0
+axios.defaults.validateStatus = status => status < 500
+
 const Communication = {
-  async post(url: string, data: any, cookies?: any): Promise<any> {
-    let options: any = {
-      method: 'POST',
-      url,
-      data,
-      maxRedirects: 0,
-      headers: {
-        'X-Requested-With': 'XMLHTTPRequest'
-      },
-      validateStatus(status: number) {
-        return status < 500
-      }
-    }
-    if (cookies) {
-      options = { ...options, withCredentials: true }
-    }
+  async post(url: string, data: any, cookie?: string[]): Promise<any> {
+    let options: any = {}
 
-    return new Promise((resolve, reject) => {
-      axios(options).then(async data => {
-        const status = data.status
-        if (status === 200 || ((Math.floor(status / 100)) === 3)) {
-          const cookie = data.headers['set-cookie']
-          if (cookie) {
-            await Config.set({ cookies: cookie[0] })
-            return resolve(data)
-          }
-        } else switch (status) {
-          case 401: return reject(new UnauthorizedError(data.statusText))
-          case 403: return reject(new ForbiddenError(data.statusText))
-          default: return reject(new InternalServerError(data.statusText))
-        }
-      }).catch(error => new UnknownError(error))
-    })
+    if (cookie) options.headers = { Cookie: cookie }
+
+    const response = await axios.post(url, data, options)
+    const { status, statusText, data: _data } = response
+    if (status === 200 || ((Math.floor(status / 100)) === 3)) {
+      await setCookie(response)
+      return _data
+    } else switch (status) {
+      case 400: throw _data.errors
+      case 401: throw (new UnauthorizedError(statusText))
+      case 403: throw (new ForbiddenError(statusText))
+      case 500: throw (new InternalServerError(statusText))
+      default: throw new UnknownError(statusText)
+    }
   },
 
-  async get(url: string, cookies?: string): Promise<boolean> {
-    return new Promise((resolve, reject) => {
-      const options: any = {}
-      if (cookies) {
-        options.headers = { cookie: cookies }
-      }
-      request.get(url, options, (err, xhr, body) => {
-        if (err) {
-          reject(err)
-          return
-        }
-        if (xhr.statusCode === 200) {
-          const setCookies = xhr.headers['set-cookie']
-          if (setCookies) {
-            Config.set({ cookies: setCookies[0] }).then(() => {
-              resolve(body === 'true')
-            }).catch(error => reject({ error }))
-          } else resolve(body === 'true')
-          return
-        }
-        reject()
-      })
-    })
+  async get(url: string, cookie?: string): Promise<boolean> {
+    const options: any = {}
+
+    if (cookie) options.headers = { Cookie: cookie }
+
+    const response = await axios.get(url, options)
+    const { status, data } = response
+    if (status === 200) await setCookie(response)
+    return data
   },
+}
+
+async function setCookie(response: AxiosResponse) {
+  const cookie = response.headers['set-cookie']
+  if (cookie) await Config.set({ cookie: cookie[0] })
 }
 
 export default Communication
