182cdd210db87dfe23ba0b606d3b4a887c774c82
Takeshi Iwana <iwatakeshi@gmail.com>
2019-11-10 01:48:51 -0700

    Remove gittar

--- src/gittar.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'gittar' {
-  import { Stream } from "stream";
-  export function fetch(repository: string): Promise<string>
-  export function extract(source: string, destination: string, options?: {
-    filter(path: string, entry: Stream): boolean
-  }): void
-}
