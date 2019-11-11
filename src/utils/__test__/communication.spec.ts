349fde50e0b354c3e306e2412412536967f18eaf
Takeshi Iwana <iwatakeshi@gmail.com>
2019-11-11 14:00:39 -0700

    Fix tests for communication.ts

--- src/utils/__test__/communication.spec.ts
+++ src/utils/__test__/communication.spec.ts
@@ -17,17 +17,20 @@ describe('utils/communication', () => {
   })
 
   describe('post()', () => {
-    it('should perform a post action without errors', () => {
-      // tslint:disable-next-line: no-floating-promises
-      expect(Communication.post('https://prismic.io/authentication/signin', {
-        email: process.env.EMAIL,
-        password: process.env.PASSWORD
-      })).rejects.not.toThrow()
+    it('should perform a post action', () => {
+      if (process.env.EMAIL && process.env.PASSWORD) {
+        return expect(Communication.post('https://prismic.io/authentication/signin', {
+          email: process.env.EMAIL,
+          password: process.env.PASSWORD
+        })).resolves.not.toBeUndefined()
+      } else {
+        // tslint:disable-next-line: no-console
+        console.warn('[skipped]: should perform a post action without errors')
+      }
     })
 
     it('should throw an error on an invalid post action', () => {
-      // tslint:disable-next-line: no-floating-promises
-      expect(Communication.post('https://prismic.io/authentication/signin', {
+      return expect(Communication.post('https://prismic.io/authentication/signin', {
         email: 'john.doe123@example.com',
         password: '12345678'
       })).rejects.toThrow()
