b402bb452e151130f9909c24ed1a9b94e4c841cc
Takeshi Iwana <iwatakeshi@gmail.com>
2019-11-11 12:50:53 -0700

    Add support for other package managers

--- src/utils/package.ts
+++ src/utils/package.ts
@@ -1,15 +1,15 @@
+import { existsSync } from 'fs'
 import { platform } from 'os'
 import { basename, join } from 'path'
 import { cd, echo, exec } from 'shelljs'
-import { existsSync } from 'fs'
 
 const Package = {
-  install(directory: string) {
+  install(directory: string, manager: 'npm' | 'yarn' = 'npm') {
     const devnull = /^win/i.test(platform()) ? 'NUL' : '/dev/null 2>&1'
     cd(directory)
     if (existsSync(join(directory, 'package.json'))) {
       echo('Installing project dependencies...')
-      exec(`npm install > ${devnull}`)
+      exec(`${manager} install > ${devnull}`)
       echo('Your project is ready, to proceed:')
       echo()
       echo(`Run 'cd ${basename(directory)}'`)
