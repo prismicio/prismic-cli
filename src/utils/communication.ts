61d958ca019890395484ceb903e58507b4015a49
Takeshi Iwana <iwatakeshi@gmail.com>
2019-11-11 20:40:39 -0700

    A little more refactoring

--- src/utils/communication.ts
+++ src/utils/communication.ts
@@ -1,4 +1,5 @@
-import got, { Options as GotOptions, Response } from 'got'
+import got, { Options as GotOptions } from 'got'
+import { IncomingHttpHeaders } from 'http2'
 
 import Config from './config'
 import {
@@ -8,16 +9,14 @@ import {
 
 const Communication = {
   async post(url: string, data: any, cookie?: string): Promise<any> {
-    let options: GotOptions & { stream?: false | undefined } = {}
+    let options: GotOptions & { stream?: false | undefined } = {
+      form: data, followRedirect: false, throwHttpErrors: false, headers: cookie ? { cookie } : {}
+    }
 
-    if (cookie) options.headers = { cookie }
-    options.form = data
-    options.followRedirect = false
-    const response = (await got.post(url, options))
-    const { statusCode: status, statusMessage: message } = response
+    const { statusCode: status, statusMessage: message, body, headers } = (await got.post(url, options))
     if (status === 200 || ((Math.floor(status / 100)) === 3)) {
-      await setCookie(response)
-      return response.body
+      await setCookie(headers)
+      return body
     } else switch (status) {
       case 400: throw (new BadRequestError(message!))
       case 401: throw (new UnauthorizedError(message!))
@@ -28,22 +27,21 @@ const Communication = {
   },
 
   async get(url: string, cookie?: string): Promise<boolean> {
-    const options: any = {}
+    const options: GotOptions & { stream?: false | undefined } = {
+      headers: cookie ? { cookie } : {}
+    }
 
-    if (cookie) options.headers = { cookie }
-
-    const response = (await got.get(url, options))
-    const { statusCode: status, body } = response
+    const { statusCode: status, body, headers } = (await got.get(url, options))
     if (status === 200) {
-      await setCookie(response)
+      await setCookie(headers)
       return body === 'true'
     }
     return false
   },
 }
 
-async function setCookie(response: Response) {
-  const cookie = response.headers['set-cookie']
+async function setCookie(headers: IncomingHttpHeaders) {
+  const cookie = headers['set-cookie']
   if (cookie) await Config.set({ cookie: cookie[0] })
 }
 
