68b9334301b867b6ecf97daa333317730316eeaf
Hugo Villain <hugo.villain@prismic.io>
2020-05-04 15:56:52 +0200

    Add create-slice

---
+++ lib/commands/slicemachine/commands/create-slice/templates/nuxt/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="section">
+    <h1>{{componentName}}</h1>
+    <p>Update it on Prismic and here ✌️</p>
+  </section>
+</template>
+
+<style scoped>
+.section {
+  max-width: 940px;
+  margin: auto;
+  text-align: center;
+}
+</style>
