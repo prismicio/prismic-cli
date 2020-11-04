668edd7f9b858865f0f1f3841e6b0df2c5843334
MarcMcIntosh <marcmcintosh1987@gmail.com>
2020-11-04 02:21:54 +0100

    test(e2e): fix theme command

--- __e2e__/theme.e2e.js
+++ __e2e__/theme.e2e.js
@@ -35,9 +35,7 @@ describe('prismic theme [ --theme-url | --folder | --conf | --template ]', () =>
       '--skip-install',
     ];
 
-    const res = spawnSync(PRISMIC_BIN, args, { encoding: 'utf8', shell: true, stdio: 'inherit' });
-    expect(res.stderr).toBeFalsy();
-    expect(res.stdout).toBeTruthy();
+    spawnSync(PRISMIC_BIN, args, { encoding: 'utf8', shell: true, stdio: 'inherit' });
     expect(fs.existsSync(dir)).toBe(true);
   });
 });
