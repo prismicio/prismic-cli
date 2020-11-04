a6cbbc4475b32894483821f3001c0f60ef240234
MarcMcIntosh <marcmcintosh1987@gmail.com>
2020-11-04 02:36:12 +0100

    test(e2e): fix version command

--- __e2e__/version.e2e.js
+++ __e2e__/version.e2e.js
@@ -4,11 +4,10 @@ const { PRISMIC_BIN } = require('./utils');
 describe('prismic --version', () => {
   it('should print out the current version on the cli', () => {
     const args = ['--version'];
-    const res = spawnSync(PRISMIC_BIN, args, { encoding: 'utf8', shell: true, stdio: 'inherit' });
+    const res = spawnSync(PRISMIC_BIN, args, { encoding: 'utf8', shell: true });
     // eslint-disable-next-line global-require
     const { version } = require('../package.json');
     const str = res.stdout.replace(/\r|\n/g, '');
     expect(str).toBe(version);
-    expect(res.stderr).toBeFalsy();
   });
 });
