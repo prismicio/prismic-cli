b6eb1a35c58298063dd925bf38b2de1adb2711ab
MarcMcIntosh <marcmcintosh1987@gmail.com>
2021-02-01 14:11:23 +0100

    chore: remove copy of old code

--- old/lib/common/utils/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Objects } from './objects';
-export { default as Files } from './files';
-export { default as Folders } from './folders';
-export { default as ziputils } from './ziputils';
