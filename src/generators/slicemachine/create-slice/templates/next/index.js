d8a054228b140b076654bacb40020528de180edb
Phil Snow <phil.snow@prismic.io>
2021-03-22 17:20:40 +0100

    added jsx to nextjs template

--- src/generators/slicemachine/create-slice/templates/next/index.js
+++ src/generators/slicemachine/create-slice/templates/next/index.js
@@ -2,28 +2,30 @@ import React from 'react';
 import { array, shape } from 'prop-types';
 import { RichText } from 'prismic-reactjs';
 
-const section = {
-  maxWidth: '600px',
-  margin: '4em auto',
-  textAlign: 'center',
-};
-
-const h2 = {
-  color: '#8592e0',
-};
-
 const <%= sliceName %> = ({ slice }) => (
-  <section style={section}>
-    {
-      slice.primary.title ?
-      <RichText render={slice.primary.title}/>
-      : <h2 style={h2}>Template slice, update me!</h2>
-    }
+  <section>
+    <span className="title">
+      {
+        slice.primary.title ?
+        <RichText render={slice.primary.title}/>
+        : <h2>Template slice, update me!</h2>
+      }
+    </span>
     {
       slice.primary.description ?
       <RichText render={slice.primary.description}/>
       : <p>start by editing this slice from inside the SliceMachine builder!</p>
     }
+    <style jsx>{`
+        section {
+          max-width: 600px;
+          margin: 4em auto;
+          text-align: center;
+        }
+        .title {
+          color: #8592e0;
+        }
+    `}</style>
   </section>
 );
 
@@ -36,6 +38,5 @@ const <%= sliceName %> = ({ slice }) => (
 };
 
 <%= sliceName %>.displayName = <%= sliceName %>
-
 
 export default <%= sliceName %>;
