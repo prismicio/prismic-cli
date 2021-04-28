9a7ec01507abed3e86d4ed275ff6d4b8dfb8662f
MarcMcIntosh <marcmcintosh1987@gmail.com>
2021-04-28 13:28:56 +0200

    feat add a command to help build generators

---
+++ packages/prismic-cli/src/commands/create-generator.ts
@@ -0,0 +1,32 @@
+import {Command, flags} from '@oclif/command'
+import {createEnv} from 'yeoman-environment'
+
+export default class CreateGenerator extends Command {
+  static description = 'create a new generator'
+
+  static flags = {
+    help: flags.help({char: 'h'}),
+    // flag with a value (-n, --name=VALUE)
+    name: flags.string({char: 'n', description: 'name for the generator'}),
+    // flag with no value (-f, --force)
+    force: flags.boolean({char: 'f'}),
+
+    'skip-install': flags.boolean({
+      default: false,
+    }),
+  }
+
+  async run() {
+    const {flags} = this.parse(CreateGenerator)
+
+    const env = createEnv()
+    env.register(require.resolve('@prismicio/prismic-generator-generator'), 'generator')
+
+    return new Promise((resolve, reject) => {
+      env.run('generator', flags, error => {
+        if (error) return reject(error)
+        return resolve(null)
+      })
+    })
+  }
+}
