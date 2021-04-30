15410fe66b1738fde53acc342b19e721d7f50a44
MarcMcIntosh <marcmcintosh1987@gmail.com>
2021-04-30 17:56:11 +0200

    refactor: generator generator templates

---
+++ packages/prismic-cli/test/commands/create-generator.test.ts
@@ -0,0 +1,17 @@
+// import {expect, test} from '@oclif/test'
+
+describe.skip('create-generator', () => {
+  /* test
+  .stdout()
+  .command(['create-generator'])
+  .it('runs hello', ctx => {
+    expect(ctx.stdout).to.contain('hello world')
+  })
+
+  test
+  .stdout()
+  .command(['create-generator', '--name', 'jeff'])
+  .it('runs hello --name jeff', ctx => {
+    expect(ctx.stdout).to.contain('hello jeff')
+  }) */
+})
