98401a0b41de836be358a2455d2cd7f49aeec454
MarcMcIntosh <marcmcintosh1987@gmail.com>
2021-05-10 15:43:12 +0200

    refacctor: filtering generators

--- packages/prismic-cli/src/prismic/yeoman-env.ts
+++ packages/prismic-cli/src/prismic/yeoman-env.ts
@@ -10,13 +10,25 @@ export const names = env.getGeneratorNames() // names of the generators
 
 export const meta = env.getGeneratorsMeta() // local first logic
 
-export const apps = Object.entries(meta).reduce<Record<string, GeneratorMeta>>((acc, [key, value]) => {
-  if (key.endsWith(':app')) {
-    return {...acc, [key]: value}
-  }
-  return acc
-}, {})
+function filterFor(generatorsMeta: Record<string, GeneratorMeta>, end: string): Record<string, GeneratorMeta> {
+  return Object.entries(generatorsMeta)
+  .reduce<Record<string, GeneratorMeta>>((acc, [key, value]) => {
+    if (key.endsWith(`:${end}`)) {
+      return {...acc, [key]: value}
+    }
+    return acc
+  }, {})
+}
+
+export const apps = filterFor(meta, 'app')
+
+export const hasCreateSlice = filterFor(meta, 'create-slice')
+
+export const hasSliceMachine = filterFor(meta, 'slicemachine')
+
+export const hasStorybook = filterFor(meta, 'storybook')
 
 export default env
 
-console.log({all, names, meta, apps})
+// console.log({all, names, meta, apps})
+
