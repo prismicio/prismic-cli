eeee1f5427564c143799c495613e560a525ab650
MarcMcIntosh <marcmcintosh1987@gmail.com>
2021-05-12 18:27:07 +0200

    chore: remove dead code

--- packages/prismic-cli/src/generators/slicemachine/create-slice/templates/next/index.stories.js
+++
@@ -1,8 +0,0 @@
-import Component from './';
-import model from './model';
-import mocks from './mocks.json';
-import { storiesOf } from '@storybook/react';
-
-mocks.forEach((variation) => {
-  storiesOf(model.name, Component).add(variation.name, () => <Component slice={variation} />);
-});
