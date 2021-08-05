78f7af34086cc123db14018ae1436b59cfbb7304
Arnaud Lewis <arnaud.lewis@gmail.com>
2021-08-05 12:30:00 +0200

    swap server by hapi to prevent connection issues from browser

--- packages/prismic-cli/src/utils/server.ts
+++ packages/prismic-cli/src/utils/server.ts
@@ -1,6 +1,7 @@
 import * as http from 'http'
 import cli from 'cli-ux'
 import {LogDecorations} from './logDecoration'
+import * as hapi from '@hapi/hapi'
 
 export const DEFAULT_PORT = 5555
 
@@ -36,23 +37,25 @@ export const handleRequest = (base: string, logAction: string, cb: (err?: null |
   }
 }
 
-export async function startServerAndOpenBrowser(
+export async function startServerAndOpenBrowser1(
   url: string,
   base: string,
   port: number,
   logAction: string,
   setCookies: (cookies?: Array<string>) => Promise<void>,
 ): Promise<void> {
+  console.log("START SERVER FN")
+  console.log(url)
   return new Promise((resolve, reject) => {
     // TODO: the error,user callabck is very difficult to reach in a test scenario, maybe this should be hoisted to the caller, or the callback should be extracted
     const server = http.createServer(handleRequest(base, logAction, (error, user) => {
       if (error || !user || !user.cookies) reject(error)
 
-      return setCookies(user?.cookies)
+      setCookies(user?.cookies)
       .then(() => {
         cli.action.stop(`Logged in as ${user?.email}`)
         server.close(error2 => {
-          if (error) return reject(error2)
+          if (error) reject(error2)
           resolve()
         })
       })
@@ -78,5 +81,76 @@ export async function startServerAndOpenBrowser(
     })
   })
 }
+
+export async function startServerAndOpenBrowser(
+  url: string,
+  base: string,
+  port: number,
+  logAction: string,
+  setCookies: (cookies: ReadonlyArray<string>) => Promise<void>
+):  Promise<void> {
+  return new Promise( async ( resolve, reject ) => {
+    const server = hapi.server({
+      port,
+      host: "localhost"
+    } );
+
+    server.route({
+      method: 'POST',
+      path: "/",
+      options: {
+        cors: {
+          origin: [base],
+          headers: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept']
+        }
+      },
+      handler: async (request, h) => {
+        try {
+          const data: { email: unknown, cookies: ReadonlyArray<string> } | null = (() => {
+            const payload = request.payload as any
+            if(payload.email && payload.cookies) return payload as { email: unknown, cookies: ReadonlyArray<string> }
+            return null
+          })()
+
+          if(!data) {
+            cli.action.stop('It seems the server didn\'t respond properly, please contact us.')
+            return h.response('Error with cookies').code(400)
+          } else {
+            try {
+              await setCookies(data.cookies)
+              cli.action.stop(`Logged in as ${data.email}`)
+              return h.response(data).code(200)
+            } catch(errorSetCookie) {
+              cli.action.stop('It seems an error happened while setting your cookies.')
+              console.error(errorSetCookie)
+              return h.response('Oops! Something wrong happened while logging.').code(500)
+            }
+          }
+        } catch (err) {
+          cli.action.stop('It seems an error happened while setting your cookies.')
+          reject(err);
+        } finally {
+          server.stop({ timeout: 10000 }).then(function (err: any) {
+            process.exit((err) ? 1 : 0)
+          })
+        }
+      }
+    });
+    
+    server.route({
+      method: ['GET','POST'],
+      path: '/{any*}',
+      handler: (request, h) => {
+        return h.response(`not found: [${request.method}]: ${request.url.toString()}`).code(404)
+      }
+    });
+    
+    await server.start();
+
+    cli.log('\nOpening browser to ' + LogDecorations.Underscore + url + LogDecorations.Reset)
+    cli.action.start(logAction, 'Waiting for the browser response')
+    cli.open(url)
+  } );
+};
 
 export default startServerAndOpenBrowser
