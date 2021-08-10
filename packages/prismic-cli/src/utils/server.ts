85c447016121b5f50a9448127c5ad339fc41567b
Arnaud Lewis <arnaud.lewis@gmail.com>
2021-08-10 15:44:38 +0200

    fix linter issues + tests

--- packages/prismic-cli/src/utils/server.ts
+++ packages/prismic-cli/src/utils/server.ts
@@ -4,36 +4,33 @@ import * as hapi from '@hapi/hapi'
 
 export const DEFAULT_PORT = 5555
 
-type HandlerData = { email: unknown, cookies: ReadonlyArray<string> }
+type HandlerData = { email: unknown; cookies: ReadonlyArray<string> }
 
 const authenticationHandler = (server: hapi.Server) => (onSuccess: (data: HandlerData, msg: string) => Promise<void>, onFailure: (error: Error, msg: string) => void) => {
   return async (request: hapi.Request, h: hapi.ResponseToolkit) => {
     try {
       const data: HandlerData | null = (() => {
         const payload = request.payload as any
-        if(payload.email && payload.cookies) return payload as { email: unknown, cookies: ReadonlyArray<string> }
+        if (payload.email && payload.cookies) return payload as { email: unknown; cookies: ReadonlyArray<string> }
         return null
       })()
 
-      if(!data) {
+      if (!data) {
         cli.action.stop('It seems the server didn\'t respond properly, please contact us.')
         return h.response('Error with cookies').code(400)
-      } else {
-        try {
-          await onSuccess(data, `Logged in as ${data.email}`)
-          return h.response(data).code(200)
-        } catch(error) {
-          onFailure(error, 'It seems an error happened while setting your cookies.')
-          return h.response('Oops! Something wrong happened while logging.').code(500)
-        }
       }
-    } catch (err) {
-      onFailure(err, 'It seems an error happened while setting your cookies.')
+      try {
+        await onSuccess(data, `Logged in as ${data.email}`)
+        return h.response(data).code(200)
+      } catch (error) {
+        onFailure(error, 'It seems an error happened while setting your cookies.')
+        return h.response('Oops! Something wrong happened while logging.').code(500)
+      }
+    } catch (error) {
+      onFailure(error, 'It seems an error happened while setting your cookies.')
       return h.response('Oops! Something wrong happened while logging.').code(500)
     } finally {
-      server.stop({ timeout: 10000 }).then(function (err: any) {
-        process.exit((err) ? 1 : 0)
-      })
+      await server.stop({timeout: 10000})
     }
   }
 }
@@ -41,33 +38,33 @@ const authenticationHandler = (server: hapi.Server) => (onSuccess: (data: Handle
 export const Routes = {
   authentication: (server: hapi.Server) => (onSuccess: (data: HandlerData, msg: string) => Promise<void>, onFailure: (error: Error, msg: string) => void) => ({
     method: 'POST',
-    path: "/",
-    handler: authenticationHandler(server)(onSuccess, onFailure)
+    path: '/',
+    handler: authenticationHandler(server)(onSuccess, onFailure),
   }),
 
   notFound: {
-    method: ['GET','POST'],
+    method: ['GET', 'POST'],
     path: '/{any*}',
     handler: (request: hapi.Request, h: hapi.ResponseToolkit) => {
       return h.response(`not found: [${request.method}]: ${request.url.toString()}`).code(404)
-    }
-  }
+    },
+  },
 }
 
 export const Server = {
-  build: (base:string, port: number, host: string) => {
+  build: (base: string, port: number, host: string) => {
     const server = hapi.server({
       port,
       host,
       routes: {
         cors: {
           origin: [base],
-          headers: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept']
-        }
-      }
-    });
+          headers: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept'],
+        },
+      },
+    })
     return server
-  }
+  },
 }
 
 export async function startServerAndOpenBrowser(
@@ -75,10 +72,9 @@ export async function startServerAndOpenBrowser(
   base: string,
   port: number,
   logAction: string,
-  setCookies: (cookies: ReadonlyArray<string>) => Promise<void>
+  setCookies: (cookies: ReadonlyArray<string>) => Promise<void>,
 ):  Promise<void> {
-  return new Promise( async ( resolve, reject ) => {
-
+  return new Promise((resolve, reject) => {
     async function onSuccess(data: HandlerData, msg: string) {
       await setCookies(data.cookies)
       cli.action.stop(msg)
@@ -93,12 +89,13 @@ export async function startServerAndOpenBrowser(
     const server = Server.build(base, port, 'localhost')
     server.route([Routes.authentication(server)(onSuccess, onFailure), Routes.notFound])
 
-    await server.start();
-
-    cli.log('\nOpening browser to ' + LogDecorations.Underscore + url + LogDecorations.Reset)
-    cli.action.start(logAction, 'Waiting for the browser response')
-    cli.open(url)
-  } );
-};
+    server.start()
+    .then(() => {
+      cli.log('\nOpening browser to ' + LogDecorations.Underscore + url + LogDecorations.Reset)
+      cli.action.start(logAction, 'Waiting for the browser response')
+      cli.open(url)
+    })
+  })
+}
 
 export default startServerAndOpenBrowser
--- packages/prismic-cli/test/utils/server.test.ts
+++ packages/prismic-cli/test/utils/server.test.ts
@@ -1,173 +1,159 @@
 import {test, expect} from '@oclif/test'
-import { Server, Routes } from '../../src/utils/server'
+import {Server, Routes} from '../../src/utils/server'
 import * as sinon from 'sinon'
-import {IncomingMessage, ServerResponse} from 'http'
-import cli from 'cli-ux'
-import * as EventEmitter from 'events'
-import * as http from 'http'
-import * as hapi from '@hapi/hapi'
-
-class Req extends EventEmitter {
-  method: string
-
-  constructor(method: 'GET' | 'OPTIONS' | 'POST') {
-    super()
-    this.method = method
-  }
-}
+// import cli from 'cli-ux'
 
 describe('server', () => {
-  let server: hapi.Server
+  const server = Server.build('https://prismic.io', 5555, 'localhost')
   const fakeOnSuccess = sinon.fake()
   const fakeOnFailure = sinon.fake()
+  server.route([Routes.authentication(server)(fakeOnSuccess, fakeOnFailure), Routes.notFound])
 
   beforeEach(async () => {
-    server = Server.build('https://prismic.io', 5555, 'localhost')
-    server.route([Routes.authentication(server)(fakeOnSuccess, fakeOnFailure), Routes.notFound])
-  });
+    await server.initialize()
+  })
 
   afterEach(async () => {
-      await server.stop();
-  });
-
-  it('returns a 404 for all other methods', async () => {
+    await server.stop()
+  })
+  test.it('returns a 404 for all other methods', async () => {
     const res = await server.inject({
-      method: 'get',
-      url: '/random'
-    });
-
+      method: 'GET',
+      url: '/random',
+    })
     expect(res.statusCode).to.equal(404)
     expect(res.payload).to.equals('not found: [get]: http://localhost:5555/random')
-  });
+  })
 
-  it('handles valid POST request', async () => {
+  test.it('Valid authentication POST request', async () => {
     const fakeCookie = 'SESSION=session-token; prismic-auth=auth-token; Path=/; Domain=.prismic.io; Secure; SameSite=None'
     const fakeEmail = 'batman@example.com'
 
     const res = await server.inject({
-      method: 'post',
+      method: 'POST',
       url: '/',
       payload: {
         email: fakeEmail,
-        cookies: fakeCookie
-      }
-    });
+        cookies: fakeCookie,
+      },
+    })
 
     expect(res.statusCode).to.equal(200)
-    // expect(fakeOnSuccess.called, 'onSuccess should be called').to.be.true
-    // expect(fakeOnFailure.called, 'onFailure should not be called').to.be.false
-    // expect(fakeOnSuccess.args[0], 'callback should be called with correct args').to.deep.equal([{cookies: fakeCookie, email: fakeEmail}, `Logged n as ${fakeEmail}`])
-  });
-
-  /* it('handles valid POST request', () => {
-    const setHeader = sinon.fake()
-    const end = sinon.fake.yields()
-    const fakeStart = sinon.fake()
-    const fakeStop = sinon.fake()
-    const res = {setHeader, end} as unknown as ServerResponse
-
-    const req = new Req('POST') as IncomingMessage
-
-    const fakeCallBack = sinon.fake()
-    const base = 'https://prismic.io'
-    const logAction = 'test'
-    const fakeCookie = 'SESSION=session-token; prismic-auth=auth-token; Path=/; Domain=.prismic.io; Secure; SameSite=None'
-    const fakeEmail = 'batman@example.com'
-
-    const startStub = sinon.stub(cli.action, 'start').callsFake(fakeStart)
-    const stopStub = sinon.stub(cli.action, 'stop').callsFake(fakeStop)
-
-    handleRequest(base, logAction, fakeCallBack)(req, res)
-    req.emit('data', JSON.stringify({cookies: fakeCookie, email: fakeEmail}))
-    req.emit('end')
-    expect(fakeStart.called, 'cli.action.start should be called').to.be.true
-    //  expect(fakeStop.called, 'cli.action.stop should be called').to.be.true
-    expect(end.called, 'res.end should be called').to.be.true
-    expect(res.statusCode).to.equal(200)
-    expect(fakeCallBack.called, 'callback should be called').to.be.true
-    expect(fakeCallBack.args[0], 'callback should be called with correct args').to.deep.equal([null, {cookies: fakeCookie, email: fakeEmail}])
-
-    startStub.reset()
-    stopStub.reset()
-  })
-
-  it('handles invalid POST request', () => {
-    const setHeader = sinon.fake()
-    const end = sinon.fake()
-    const res = {setHeader, end} as unknown as ServerResponse
-
-    const req = new Req('POST') as IncomingMessage
-
-    const fakeCallBack = sinon.fake()
-    const base = 'https://prismic.io'
-    const logAction = 'test'
-
-    handleRequest(base, logAction, fakeCallBack)(req, res)
-    req.emit('data', JSON.stringify({}))
-    req.emit('end')
-
-    expect(end.called).to.be.true
-    expect(res.statusCode).to.equal(400)
-    expect(fakeCallBack.called).to.be.false
+    expect(fakeOnSuccess.called, 'onSuccess should be called').to.be.true
+    expect(fakeOnFailure.called, 'onFailure should not be called').to.be.false
+    expect(fakeOnSuccess.args[0], 'callback should be called with correct args').to.deep.equal([{cookies: fakeCookie, email: fakeEmail}, `Logged in as ${fakeEmail}`])
   })
 })
 
-describe('server', () => {
-  beforeEach(() => {
-    sinon.reset()
-  })
+//   it('handles valid POST request', () => {
+//     const setHeader = sinon.fake()
+//     const end = sinon.fake.yields()
+//     const fakeStart = sinon.fake()
+//     const fakeStop = sinon.fake()
+//     const res = {setHeader, end} as unknown as ServerResponse
 
-  const fakeStart = sinon.fake()
-  const fakeStop = sinon.fake()
-  const fakeOpen = sinon.fake.resolves(null)
+//     const req = new Req('POST') as IncomingMessage
 
-  const fakeSetCookiesSuccess = sinon.fake.resolves(null)
+//     const fakeCallBack = sinon.fake()
+//     const base = 'https://prismic.io'
+//     const logAction = 'test'
+//     const fakeCookie = 'SESSION=session-token; prismic-auth=auth-token; Path=/; Domain=.prismic.io; Secure; SameSite=None'
+//     const fakeEmail = 'batman@example.com'
 
-  class FakeServer extends EventEmitter  {
-    listen = sinon.fake.yields(this)
+//     const startStub = sinon.stub(cli.action, 'start').callsFake(fakeStart)
+//     const stopStub = sinon.stub(cli.action, 'stop').callsFake(fakeStop)
 
-    close = sinon.fake()
-  }
+//     handleRequest(base, logAction, fakeCallBack)(req, res)
+//     req.emit('data', JSON.stringify({cookies: fakeCookie, email: fakeEmail}))
+//     req.emit('end')
+//     expect(fakeStart.called, 'cli.action.start should be called').to.be.true
+//     //  expect(fakeStop.called, 'cli.action.stop should be called').to.be.true
+//     expect(end.called, 'res.end should be called').to.be.true
+//     expect(res.statusCode).to.equal(200)
+//     expect(fakeCallBack.called, 'callback should be called').to.be.true
+//     expect(fakeCallBack.args[0], 'callback should be called with correct args').to.deep.equal([null, {cookies: fakeCookie, email: fakeEmail}])
 
-  const fakeHttpServer = new FakeServer()
+//     startStub.reset()
+//     stopStub.reset()
+//   })
 
-  test
-  .stub(cli, 'open', () => fakeOpen)
-  .stub(cli.action, 'start', fakeStart)
-  .stub(cli.action, 'stop', fakeStop)
-  .stub(cli, 'log', sinon.fake())
-  .stub(http, 'createServer', () => fakeHttpServer)
-  .it('startServerAndOpenBrowser success case', async (_, done) => {
-    server('https://prismic.io/test', 'https://prismic.io', 5555, 'logActions', fakeSetCookiesSuccess)
-    fakeHttpServer.emit('data', JSON.stringify({cookies: 'foo', email: 'test@example.com'}))
-    fakeHttpServer.emit('end')
+//   it('handles invalid POST request', () => {
+//     const setHeader = sinon.fake()
+//     const end = sinon.fake()
+//     const res = {setHeader, end} as unknown as ServerResponse
 
-    expect(fakeHttpServer.listen.firstCall.args[0]).to.equal(5555)
-    expect(fakeStart.called, 'should call cli.action.start').to.be.true
-    // expect(fakeStop.called, 'should call cli.action.stop').to.be.true
-    expect(fakeOpen.called, 'should call cli.open').to.be.true
-    expect(fakeOpen.firstCall.args, 'should open the prvided url').to.contain('https://prismic.io/test')
-    // expect(fakeSetCookiesSuccess.called).to.be.true
-    // expect(fakeStart.firstCall.args[0]).to.equal('logActions')
-    done()
-  })
+//     const req = new Req('POST') as IncomingMessage
 
-  // const catchFn = sinon.fake()
+//     const fakeCallBack = sinon.fake()
+//     const base = 'https://prismic.io'
+//     const logAction = 'test'
 
-  test
-  .stub(cli, 'open', () => fakeOpen)
-  .stub(cli.action, 'start', fakeStart)
-  .stub(cli.action, 'stop', fakeStop)
-  .stub(cli, 'log', sinon.fake())
-  .stub(http, 'createServer', () => fakeHttpServer)
-  .stderr()
-  .it('startServerAndOpenBrowser error case', async (ctx, done) => {
-    server('https://prismic.io/test', 'https://prismic.io', 5555, 'logActions', () => Promise.resolve())
-    fakeHttpServer.emit('error', 'foo')
-    expect(fakeStop.called, 'cli.action.stop should be called').to.be.true
-    expect(fakeHttpServer.close.called, 'server.close should be called').to.be.true
-    expect(ctx.stderr, 'sdterr should contain error message').to.contain('foo')
-    // expect(catchFn.called, 'server promise should reject').to.be.true
-    done()
-  }) */
-})
+//     handleRequest(base, logAction, fakeCallBack)(req, res)
+//     req.emit('data', JSON.stringify({}))
+//     req.emit('end')
+
+//     expect(end.called).to.be.true
+//     expect(res.statusCode).to.equal(400)
+//     expect(fakeCallBack.called).to.be.false
+//   })
+// })
+
+// describe('server', () => {
+//   beforeEach(() => {
+//     sinon.reset()
+//   })
+
+//   const fakeStart = sinon.fake()
+//   const fakeStop = sinon.fake()
+//   const fakeOpen = sinon.fake.resolves(null)
+
+//   const fakeSetCookiesSuccess = sinon.fake.resolves(null)
+
+//   class FakeServer extends EventEmitter  {
+//     listen = sinon.fake.yields(this)
+
+//     close = sinon.fake()
+//   }
+
+//   const fakeHttpServer = new FakeServer()
+
+//   test
+//   .stub(cli, 'open', () => fakeOpen)
+//   .stub(cli.action, 'start', fakeStart)
+//   .stub(cli.action, 'stop', fakeStop)
+//   .stub(cli, 'log', sinon.fake())
+//   .stub(http, 'createServer', () => fakeHttpServer)
+//   .it('startServerAndOpenBrowser success case', async (_, done) => {
+//     server('https://prismic.io/test', 'https://prismic.io', 5555, 'logActions', fakeSetCookiesSuccess)
+//     fakeHttpServer.emit('data', JSON.stringify({cookies: 'foo', email: 'test@example.com'}))
+//     fakeHttpServer.emit('end')
+
+//     expect(fakeHttpServer.listen.firstCall.args[0]).to.equal(5555)
+//     expect(fakeStart.called, 'should call cli.action.start').to.be.true
+//     // expect(fakeStop.called, 'should call cli.action.stop').to.be.true
+//     expect(fakeOpen.called, 'should call cli.open').to.be.true
+//     expect(fakeOpen.firstCall.args, 'should open the prvided url').to.contain('https://prismic.io/test')
+//     // expect(fakeSetCookiesSuccess.called).to.be.true
+//     // expect(fakeStart.firstCall.args[0]).to.equal('logActions')
+//     done()
+//   })
+
+//   // const catchFn = sinon.fake()
+
+//   test
+//   .stub(cli, 'open', () => fakeOpen)
+//   .stub(cli.action, 'start', fakeStart)
+//   .stub(cli.action, 'stop', fakeStop)
+//   .stub(cli, 'log', sinon.fake())
+//   .stub(http, 'createServer', () => fakeHttpServer)
+//   .stderr()
+//   .it('startServerAndOpenBrowser error case', async (ctx, done) => {
+//     server('https://prismic.io/test', 'https://prismic.io', 5555, 'logActions', () => Promise.resolve())
+//     fakeHttpServer.emit('error', 'foo')
+//     expect(fakeStop.called, 'cli.action.stop should be called').to.be.true
+//     expect(fakeHttpServer.close.called, 'server.close should be called').to.be.true
+//     expect(ctx.stderr, 'sdterr should contain error message').to.contain('foo')
+//     // expect(catchFn.called, 'server promise should reject').to.be.true
+//     done()
+//   })
+// })
