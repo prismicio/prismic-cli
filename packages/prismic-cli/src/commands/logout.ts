ee370d75f6569bcfed398b60afd238dd382fbf90
Pau <32859437+pvpg@users.noreply.github.com>
2021-08-17 16:11:06 -0500

    Update logout.ts

--- packages/prismic-cli/src/commands/logout.ts
+++ packages/prismic-cli/src/commands/logout.ts
@@ -2,7 +2,7 @@ import {flags} from '@oclif/command'
 import {Command} from '../prismic'
 
 export default class Logout extends Command {
-  static description = 'logout of prismic'
+  static description = 'Logout of Prismic.'
 
   static flags = {
     help: flags.help({char: 'h'}), // -h causes errors?
